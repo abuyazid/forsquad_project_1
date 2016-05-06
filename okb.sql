7d2cd65d585bd86894263199add6ba255abd4c8f
Fahrur <mohfahrurrizqon@gmail.com>
2016-05-06 23:47:34 +0700

    tes2

--- okb.sql
+++ okb.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `okb`
+-- Database: `okb     `
 --
 CREATE DATABASE IF NOT EXISTS `okb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `okb`;
